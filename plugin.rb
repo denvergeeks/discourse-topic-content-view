67c35679f53e83bcf4e3e5502b1711a713a05caa
denvergeeks <jen@denvergeeks.com>
2026-03-04 07:35:21 -0700

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -1,32 +1,55 @@
-# name: discourse-published-pages-cooked
-# about: Renders published pages as cooked content instead of static HTML
+# name: discourse-topic-content-view
+# about: Display topic content in a clean view with just cooked content
 # version: 0.1.0
 # authors: @denvergeeks
-# url: https://github.com/denvergeeks/discourse-published-pages-cooked
+# url: https://github.com/denvergeeks/discourse-topic-content-view
 
-enabled_site_setting :published_pages_cooked_enabled
+enabled_site_setting :topic_content_view_enabled
 
 after_initialize do
-  require_dependency 'published_page'
-  
-  PublishedPage.class_eval do
-    def contents_excerpt
-      PrettyText.cook(contents)
+  module ::DiscourseTopicContentView
+    class Engine < ::Rails::Engine
+      engine_name "discourse_topic_content_view"
+      isolate_namespace DiscourseTopicContentView
+    end
+  end
+
+  class DiscourseTopicContentView::TopicContentController < ::ApplicationController
+    requires_plugin 'discourse-topic-content-view'
+    
+    skip_before_action :check_xhr, :preload_json, :verify_authenticity_token
+    
+    def show
+      # Get topic by ID or slug
+      topic = find_topic(params[:id])
+      
+      raise Discourse::NotFound unless topic
+      
+      # Check permissions
+      guardian.ensure_can_see!(topic)
+      
+      # Get the first post (topic content)
+      @post = topic.first_post
+      raise Discourse::NotFound unless @post
+      
+      # Render with minimal layout
+      render :show, layout: 'topic_content'
+    end
+    
+    private
+    
+    def find_topic(id_or_slug)
+      # Try to find by ID first
+      if id_or_slug.to_i.to_s == id_or_slug
+        Topic.find_by(id: id_or_slug.to_i)
+      else
+        # Find by slug
+        Topic.find_by(slug: id_or_slug)
+      end
     end
   end
   
-  # Override the published page controller to use cooked content
-  require_dependency 'published_pages_controller'
-  
-  PublishedPagesController.class_eval do
-    def show
-      @published_page = PublishedPage.find_by(slug: params[:slug].downcase)
-      raise Discourse::NotFound unless @published_page
-      
-      # Cook the content instead of using static HTML
-      @published_page_contents = PrettyText.cook(@published_page.contents)
-      
-      render :show, layout: 'publish'
-    end
+  Discourse::Application.routes.append do
+    get '/topic-content/:id' => 'discourse_topic_content_view/topic_content#show', constraints: { id: /[^\/]+/ }
   end
 end
